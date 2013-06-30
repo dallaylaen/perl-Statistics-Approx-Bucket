2524c009112d5d2593a1ef25b84ccedfff0c0341
Konstantin S. Uvarin <khedin@gmail.com>
2013-06-30 17:38:13 +0400

    Update dist.sh NO CODE CHANGE

--- dist.sh
+++ dist.sh
@@ -3,9 +3,14 @@
 PERLS="5.6.0 5.8.8 5.18.0"
 
 die () {
-	echo "FATAL: $1" >&2
+	echo "FATAL:" $1 >&2
 	exit 1
 }
+
+# only can make dist on commit border
+[ `git symbolic-ref HEAD | sed 's#^.*/##'` = "master" ] ||\
+	die "Must be on master for dist"
+[ -z "`git status -s`" ] || die "Uncommitted changes present"
 
 perlbrew switch-off
 perl --version | grep "^This is"
@@ -17,9 +22,19 @@ VER=`perl -MYAML=LoadFile -we 'print LoadFile(shift)->{version}' MYMETA.yml`
 
 echo "Version $VER..."
 
-# only can make dist on commit border
-[ -z "`git status -s`" ] || die "Uncommitted changes present"
+USED=`grep -hiro '^use *[a-z][a-z0-9:_]*' lib t bin 2>/dev/null | awk '{print $2}' | sort -u `
 
+echo "Use:" $USED
+
+USED=`perl -MYAML=LoadFile -wle '$c=LoadFile("MYMETA.yml"); 
+	for(@ARGV) { exists $c->{requires}{$_} || exists $c->{build_requires}{$_} 
+	|| /^(strict|warnings|fields)$/ || print $_ }' $USED`
+
+USED=`perl -wle 'for(@ARGV) { $m=$_; s#::#/#g; -f "lib/$_.pm" or print $m }' $USED`
+
+[ -z "$USED" ] || die "Used modules not in prereq: $USED"
+
+exit
 # check make test on several perls
 FAILS=
 for i in $PERLS; do
@@ -27,8 +42,8 @@ for i in $PERLS; do
 	prove -I lib t/ || FAILS="$FAILS $i"
 done
 
-perlbrew switch-off
-prove -I lib t/ || FAILS="$FAILS system-perl"
+# perlbrew switch-off
+$ prove -I lib t/ || FAILS="$FAILS system-perl"
 
 [ \! -z "$FAILS" ] && die "Tests failed under perls $FAILS"
 
