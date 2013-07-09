464f5d9e21e684d8e7e11a2fe421c66a62dcfa84
Konstantin S. Uvarin <khedin@gmail.com>
2013-07-09 22:51:50 +0400

    TST png example - added some text into the pic & help

--- example/png-histogram.pl
+++ example/png-histogram.pl
@@ -21,13 +21,26 @@ GetOptions (
 	'utrim=s' => \$opt{utrim},
 	'min=s' => \$opt{min},
 	'max=s' => \$opt{max},
-	'help' => sub {
-		print "Usage: $0 [--base <1+small o> --floor <nnn>] pic.png\n";
-		print "Read numbers from STDIN, output histogram\n";
-		print "Number of sections = n (default 20)";
-		exit 2;
-	},
-);
+	'help' => \&usage,
+) or usage();
+
+sub usage {
+	print STDERR <<"EOF";
+Usage: $0 [options] pic.png
+Read numbers from STDIN, output histogram as a png file.
+Options may include:
+  --width <nnn>
+  --height <nnn> - size of picture
+  --base <1 + small o> - relative precision of underlying engine
+  --floor <positive number> - count all below this as zero
+  --ltrim <0..100> - cut that % off from the left
+  --utrim <0..100> - cut that % off from the right
+  --min <xxx> - strip data below this value
+  --max <xxx> - strip data above this value
+  --help - this message
+EOF
+	exit 2;
+};
 
 # Where to write the pic
 my $out = shift;
@@ -39,6 +52,9 @@ if ($out eq '-') {
 } else {
 	open ($fd, ">", $out) or die "Failed to open $out: $!";
 };
+
+# sane default for precision = 1 pixel at right/left side
+$opt{base} = 1+1/$opt{width} unless defined $opt{base};
 
 my $stat = Statistics::Descriptive::LogScale->new(
 	base => $opt{base}, zero_thresh => $opt{zero});
@@ -54,14 +70,21 @@ my $hist = $stat->histogram( %opt, count => $width);
 my $trimmer = Statistics::Descriptive::LogScale->new;
 $trimmer->add_data(map { $_->[0]} @$hist);
 
-my $max = $trimmer->percentile(99)/0.7;
-$_->[0] /= $max for @$hist;
+my $max_val = $trimmer->percentile(99)/0.7;
+$_->[0] /= $max_val for @$hist;
 
 # warn "hist = @hist\n";
 # draw!
 my $gd = GD::Simple->new($width, $height);
 $gd->bgcolor('white');
 $gd->clear;
+
+my $scale = 10;
+
+foreach (1 .. ($scale-1)) {
+	$gd->fgcolor( 'blue' );
+	$gd->line( $width * ($_/$scale), 0, $width * ($_/$scale), $height );
+};
 
 my $i=0;
 foreach (@$hist) {
@@ -70,5 +93,16 @@ foreach (@$hist) {
 	$i++;
 };
 
-print $fd $gd->png;
+my $min = $hist->[0][1];
+my $max = $hist->[-1][2];
+my $range = $max - $min;
 
+foreach (0 .. ($scale-1)) {
+	$gd->fgcolor( 'blue' );
+	$gd->moveTo( $width * ($_/$scale) + 2, 10 );
+	$gd->fontsize( 8 );
+	$gd->font( "Times" );
+	$gd->string( sprintf("%0.1f", $min + $range*$_/$scale) );
+};
+
+print $fd $gd->png;
